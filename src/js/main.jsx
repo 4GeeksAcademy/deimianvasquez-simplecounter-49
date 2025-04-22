f54d928e0d67ee416d9955e526e5f53a8fa243c3
Deimian Vásquez <deimianvasquez@gmail.com>
2025-04-22 01:34:05 +0000

    Counter ended

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,30 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import { SecondsCounter } from './components/SecondsCounter';
+
+// let counter = 0
+
+// setInterval(() => {
+//   counter++
+
+//   const one = Math.floor(counter / 1)
+//   const two = Math.floor(counter / 10)
+//   const three = Math.floor(counter / 100)
+//   const four = Math.floor(counter / 1000)
+
+//   ReactDOM.createRoot(document.getElementById('root')).render(
+//     <SecondsCounter
+//       numberOne={one}
+//       numberTwo={two}
+//       numberThree={three}
+//       numberFour={four}
+//     />,
+//   )
+
+// }, 100)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+  <SecondsCounter />,
 )
